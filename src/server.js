30d29aba4852f9af9abd02cef6bde067087d25f6
mitanshu5911 <mitanshubansal1234@gmail.com>
2026-02-10 12:55:58 +0530

    hello

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 import app from "./app.js";
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
